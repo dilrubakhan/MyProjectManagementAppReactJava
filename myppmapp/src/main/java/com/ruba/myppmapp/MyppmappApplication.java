e99218f3908e4a509103dc4febf77f88df04ddcd
Dilruba Khan <rubatanisa@yahoo.ie>
2021-11-13 14:42:32 +0000

    first commit

---
+++ myppmapp/src/main/java/com/ruba/myppmapp/MyppmappApplication.java
@@ -0,0 +1,13 @@
+package com.ruba.myppmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyppmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyppmappApplication.class, args);
+	}
+
+}
---
+++ myppmapp/src/test/java/com/ruba/myppmapp/MyppmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruba.myppmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyppmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
